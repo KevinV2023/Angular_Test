6437942911903ff9810d3ecc0de3e80a6cf69d5b
KevinV2023 <kevin.viannenc2023@campus-eni.fr>
2024-11-05 16:33:41 +0100

    Create component visites

---
+++ src/app/visites/visites.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visites',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      visites works!
+    </p>
+  `,
+  styleUrl: './visites.component.css'
+})
+export class VisitesComponent {
+
+}
