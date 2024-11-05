d78e390b82a02d2db5ff2e78f08b4f4ee3f985f7
KevinV2023 <kevin.viannenc2023@campus-eni.fr>
2024-11-05 16:32:21 +0100

    Create component medecins

---
+++ src/app/medecins/medecins.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medecins',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      medecins works!
+    </p>
+  `,
+  styleUrl: './medecins.component.css'
+})
+export class MedecinsComponent {
+
+}
